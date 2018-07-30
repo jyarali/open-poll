0f75171832c47cbb399aab5bd0cb4964871393c3
Jamal Yarali <jamaldanlod@gmail.com>
2018-07-30 22:48:34 +0430

    edited db name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'url' : 'mongodb://localhost:27017/jpro',     // mongodb database URL Like: mongodb://localhost:27017/jpro
-    'host': 'http://localhost:3000'               // base website URL Like: http://localhost:3000
+    'url' : 'mongodb://localhost:27017/openpoll',     // mongodb database URL Like: mongodb://localhost:27017/openpoll
+    'host': 'http://localhost:3000'                   // base website URL Like: http://localhost:3000
 }
